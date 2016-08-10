ce2f4ebcec841a08aae3712c45663291754c9838
JuliaNumberTypes <julianumbertypes@gmail.com>
2016-08-10 19:02:33 -0400

    Delete cvtother.jl

--- src/type/cvtother.jl
+++
@@ -1,8 +0,0 @@
-
-function convert(::Type{Rational{BigInt}}, x::AbstractFloat)
-    if isnan(x); return zero(BigInt)//zero(BigInt); end
-    if isinf(x); return copysign(one(BigInt),x)//zero(BigInt); end
-    if x == 0;   return zero(BigInt) // one(BigInt); end
-    s = max(precision(x) - exponent(x), 0)
-    BigInt(ldexp(x,s)) // (BigInt(1) << s)
-end
